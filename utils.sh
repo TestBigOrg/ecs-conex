b182a46add0d1521718199207d55dd8af9a730b1
Emily DuBois <emilymdubois@gmail.com>
2017-04-07 17:42:16 -0400

    More :hocho: :bug:

--- utils.sh
+++ utils.sh
@@ -133,6 +133,11 @@ function ecr_logins() {
 function ecr_cleanup() {
   local region=$1
   local repo=$2
+
+  if [ -z "$symlink" ]; then
+    file=/usr/local/bin/cleanup_ecr
+    [ -h $file ] || ln -s `pwd`/scripts/cleanup.js $file
+  fi
   cleanup_ecr ${region} ${repo}
 }
 
@@ -151,8 +156,6 @@ function docker_push() {
     fi
 
     echo "making space in the registry"
-    file=/usr/local/bin/cleanup_ecr
-    [ -h $file ] || ln -s `pwd`/scripts/cleanup.js $file
     ecr_cleanup ${region} ${repo}
 
     echo "pushing ${after} to ${region}"
